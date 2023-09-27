96cd5e61dc68fec762aa281a76e5552bf3bddc68
ZhiyuLin1 <zlin080@uottawa.ca>
2023-09-27 10:50:22 -0400

    updated

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,20 +1,5 @@
 package com.example.simplecalculator;
 
-/*
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
-*/
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
